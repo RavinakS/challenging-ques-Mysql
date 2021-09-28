28ab5c77199e31fcc2fe8c1e341ce36c79b1a4ed
RavinakS <ravinasingh697@gmail.com>
2021-09-29 01:24:46 +0530

    installed knex and now thinking to connect with db but express is left

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-## Hello world
+## Exercses
+ - 1 /registration  a user (name email, password) - post
+ - 2 /profile-edit/:userId (name email, password )-put
+ - 3 /delete-user/:userId
+
+Do this, for now, will add the stuff once you will be done with this.
--- demofile.js
+++ demofile.js
@@ -1,20 +1,23 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'ravina@123',
-    database: 'pets',
-    port: 3306
+const knex = require('knex')({
+    client: 'mysql2',
+    connection: {
+        host: 'localhost',
+        user: 'root',
+        password: 'ravina@123',
+        database: 'pets',
+        port: 3306
+    }
 });
 
-// db.connect(err => {
-//     if(err){
-//         console.log(err);
-//     }else{
-//         console.log("* * Connected!! * *");
-//     }
-// })
+db.connect(err => {
+    if(err){
+        console.log(err);
+    }else{
+        console.log("* * Connected!! * *");
+    }
+})
 
 db.query('show tables', function(err, table){
     console.log(table);
